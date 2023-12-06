4a4ee1f0a4de86ddb04d41c2e0a228cfe815a76e
Rohit Dhiman <rohitdhiman937@gmail.com>
2023-12-07 00:53:20 +0530

    few changes

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,11 +1,13 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import HomeScreen from '../screens/HomeScreen';
 const AppNavigation = () => {
 
     const Stack = createNativeStackNavigator()
     return <NavigationContainer>
-        <Stack.Screen />
+        <Stack.Navigator initialRouteName='Home'>
+            <Stack.Screen component={HomeScreen} name='Home' />
+        </Stack.Navigator>
     </NavigationContainer>
 }
 
