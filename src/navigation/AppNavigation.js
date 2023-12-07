47fa9e676cb0a3e6cf24b6db168909e1a85ad506
Rohit Dhiman <rohitdhiman937@gmail.com>
2023-12-07 15:05:48 +0530

    ui changes

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,6 +1,9 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import HomeScreen from '../screens/HomeScreen';
+
+
 const AppNavigation = () => {
 
     const Stack = createNativeStackNavigator()
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,50 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, StyleSheet, TextInput, ImageBackground, TouchableOpacity } from 'react-native'
+import { theme } from '../theme'
+
 
 const HomeScreen = () => {
+
     return (
-        <View>
-            <Text >HomeScreen</Text>
-        </View>
+        <ImageBackground source={require('../assets/bg.png')} style={styles.image} >
+            <View style={styles.container}>
+                <TextInput placeholder='Enter city' style={styles.searchContainer} placeholderTextColor={'lightgray'} />
+                <TouchableOpacity style={styles.button}>
+                </TouchableOpacity>
+            </View>
+        </ImageBackground>
+
+
+
+
     )
 }
 
+console.log("theme", theme)
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+
+    },
+    button: {
+
+    },
+    image: {
+        flex: 1,
+        resizeMode: 'cover'
+    },
+    searchContainer: {
+        padding: 4,
+        marginHorizontal: 5,
+
+        borderRadius: 5,
+        backgroundColor: theme.bgWhite(0.2)
+
+    },
+    searchInput: {
+        borderWidth: 10,
+        borderColor: 'red'
+    }
+})
 export default HomeScreen
